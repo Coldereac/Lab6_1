38b5a07b3a3bea23fa6f000d46a5559ba49531b7
Anton Malynevskyi <malina.elfg@gmail.com>
2024-04-06 21:55:27 +0200

    Initial commit

---
+++ main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+long rf(int n);
+long rf_calculation(int i, int level);
+
+int main() {
+    int n;
+    puts("Enter number");
+    scanf("%d", &n);
+    printf("For: %d! = %ld \n", n, rf(n));
+    return 0;
+}
+
+long rf_calculation(int n, int level) {
+    printf(" Level: %d\t Recursive descent:%d\n", level, n);
+    if (n == 1|| n == 0) {
+        printf(" Level: %d\t Recursive descent:%d\tCurrent value: %d\n", level, n, 1);
+        return 1;
+    }
+        long int result = n * rf_calculation(n - 1, level+1);
+        printf(" Level: %d\t Recursive descent:%d\tCurrent value: %ld\n", level, n, result);
+        return result;
+}
+
+long rf(int n) {
+    return rf_calculation(n, 1);
+}
